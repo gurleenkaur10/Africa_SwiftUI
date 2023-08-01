f468f6d44c54350b23a6cd90771d86d9f528a79b
Gurleen Kaur <gurleen.kaur101010@gmail.com>
2023-08-01 16:33:39 -0400

    Added the gallery feature with circle images in grid layout with slider.

--- Africa/App/GalleryView.swift
+++ Africa/App/GalleryView.swift
@@ -8,9 +8,71 @@
 import SwiftUI
 
 struct GalleryView: View {
+    
+    @State private var selectedAnimal : String = "lion"
+    
+    let animal: [Animal] = Bundle.main.decode("animals.json")
+    let hapticImpat = UIImpactFeedbackGenerator(style: .medium)
+    
+    // simple grid deination
+//    let gridLayout: [GridItem] = [
+//        GridItem(.flexible()),
+//        GridItem(.flexible()),
+//        GridItem(.flexible())]
+    // efficent grid
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 4)
+    //Dynamic grid layout
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColum: Double = 3.0
+    
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColum))
+    }
+    
+    
+    
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            
+            
+            VStack(alignment: .center, spacing: 30) {
+                
+                //Image
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                //Slider
+
+                Slider(value: $gridColum, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColum, perform: { value in
+                        gridSwitch()
+                    })
+                //Grid
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animal) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                                hapticImpat.impactOccurred()
+                            }
+                    }// loop
+                }// grid
+                .animation(.easeIn)
+                .onAppear(perform: {
+                  gridSwitch()
+                })
+            }// vstack
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         }// scroll
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
